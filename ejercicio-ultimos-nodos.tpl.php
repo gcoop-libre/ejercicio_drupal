31a7da7a6724ac3d6bcd241779e8901bffdc7713
mpv <ariel@gcoop.coop>
2014-07-02 17:01:35 -0300

    mostramos enlaces a nodo en el bloque

---
+++ ejercicio-ultimos-nodos.tpl.php
@@ -0,0 +1,7 @@
+<h3>Últimos <?php print $cantidad; ?> nodos de tipo <?php print $tipo; ?></h3>
+
+<ul>
+<?php foreach ($titulos as $titulo): ?>
+  <li><?php print $titulo; ?></li>
+<?php endforeach; ?>
+</ul>
